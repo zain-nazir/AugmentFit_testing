7ad70593b8b2857bd2433bd0cdcf890fd821a162
Zain Nazir <zainnazir7890@gmail.com>
2025-07-03 07:31:36 +0500

    mini change

--- web-app/src/pages/dashboard.js
+++ web-app/src/pages/dashboard.js
@@ -147,7 +147,7 @@ export default function Dashboard() {
 
   return (
     <div className="dashboard">
-      <h2 className="underline main-heading">Dashboard</h2>
+      <h2 className="underline main-heading">Dashboard Main</h2>
       {loading ? (
         <LoadingSpinner />
       ) : (
